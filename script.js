ae291c71490f4b5b1881da4e1b6f8da7159375cd
Yomary026 <ynfanteyomary@gmail.com>
2024-10-24 18:02:49 -0400

    Actualizacion

--- script.js
+++ script.js
@@ -244,9 +244,9 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         'Para el dolor': { 
             name: 'Megadol', 
-            description: 'Alivio de molestias: Reduce el malestar, mejorando la movilidad y flexibilidad. Experimenta una vida más activa. Protección del cartílago: Previene futuros daños en tus articulaciones. Control de inflamación: Propiedades antiinflamatorias pueden aliviar aún más.', 
+            description: 'Alivio del dolor rápido.', 
             quantity: '60 cápsulas',
-            ingredients: 'Glucosamina sulfato, Colageno, Metilsulfonilmetano, Mezcla propietaria antiinflamatoria, Veneno de Abeja, Condroitín sulfato',
+            ingredients: 'Ibuprofeno, Extracto de jengibre',
             usage: '1 cápsula cada 8 horas.'
         },
         'Super x natural': { 
